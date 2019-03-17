f545dac34277c8505658da817401d69a00bf2031
zhuochenhust <zhuochenuva@gmail.com>
2019-03-16 21:16:49 -0400

    Solution 110

--- src/tree/BalancedBinaryTree_110.java
+++ src/tree/BalancedBinaryTree_110.java
@@ -60,7 +60,7 @@ public class BalancedBinaryTree_110 {
 		if (root == null) return 0;
 		int leftDepth = findDepth(root.left);
 		int rightDepth = findDepth(root.right);
-		return  leftDepth > rightDepth ? leftDepth + 1 : rightDepth + 1;		
+		return  leftDepth > rightDepth ? leftDepth + 1 : rightDepth + 1;			
 	}
 	
 	/**
