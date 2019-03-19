7f2e1a7cda621b48b7126bcecc6535d285bffd7b
zhuochenhust <zhuochenuva@gmail.com>
2019-03-19 19:33:15 -0400

    Solution 116

---
+++ src/tree/PopulatingNextRightPointersInEachNode_116.java
@@ -0,0 +1,37 @@
+package tree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class PopulatingNextRightPointersInEachNode_116 {
+	// 基本思路：BFS
+	public Node connect(Node root) {
+        if (root == null) return root;
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+        	int size = queue.size();
+        	List<Node> cur = new ArrayList<>();
+        	for (int i = 0; i < size; i ++) {
+        		Node head = queue.poll();
+        		cur.add(head);
+        		if (head.left != null) {
+        			queue.offer(head.left);
+        		}
+        		if (head.right != null) {
+        			queue.offer(head.right);
+        		}
+        	}
+        	for (int i = 0; i < cur.size(); i ++) {
+        		if (i == cur.size() - 1) {
+        			cur.get(i).next = null;
+        			break;
+        		}
+        		cur.get(i).next = cur.get(i + 1);
+        	}
+        }
+        return root;
+    }
+}
