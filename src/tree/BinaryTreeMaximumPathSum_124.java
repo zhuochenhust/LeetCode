8e1150a7bb536bee5b3a00a609c44a84a52c3a04
zhuochenhust <zhuochenuva@gmail.com>
2019-03-21 21:07:47 -0400

    Solution 124

--- src/tree/BinaryTreeMaximumPathSum_124.java
+++ src/tree/BinaryTreeMaximumPathSum_124.java
@@ -11,17 +11,18 @@ public class BinaryTreeMaximumPathSum_124 {
     
     public int helper(TreeNode root) {
         if(root == null) return 0;
-        int left = helper(root.left);
-        int right = helper(root.right);
+        // 负数加上root的值只会变小，所以不是无脑取left和right的最大值，要他们都为负数，哪怕取大的加上root也拖后腿了
+        int left = Math.max(helper(root.left), 0);
+        int right = Math.max(helper(root.right), 0);
         max = Math.max(max, root.val + left + right);
-        int current = root.val + Math.max(left, right);
-        return current > 0 ? current : 0;
+        // 每条边只能用一次，所以并不是返回max，如果返回max的话，那么按照例子结果就是13了
+        return Math.max(left, right) + root.val;
     }
     
     @Test
     public void testSolution() {
-    	TreeNode root = new TreeNode(-10, new TreeNode(9, null, null), 
-    					new TreeNode(20, new TreeNode(15, null, null), new TreeNode(7, null, null)));
+    	TreeNode root = new TreeNode(0, new TreeNode(3, null, null), 
+    					new TreeNode(2, new TreeNode(4, null, null), new TreeNode(4, null, null)));
     	int res = maxPathSum(root);
     	System.out.println(res);
     }
