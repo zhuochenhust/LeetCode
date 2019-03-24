8eb9dc4ecbfd5a0a463c1ca43f171a423c9fdae8
zhuochenhust <zhuochenuva@gmail.com>
2019-03-24 17:41:17 -0400

    Solution 144

---
+++ src/tree/BinaryTreePreorderTraversal_144.java
@@ -0,0 +1,28 @@
+package tree;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class BinaryTreePreorderTraversal_144 {
+	public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) return res;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            int size = stack.size();
+            for (int i = 0; i < size; i ++) {
+                TreeNode head = stack.pop();
+                if (head.right != null) {
+                    stack.push(head.right);
+                }
+                if (head.left != null) {
+                    stack.push(head.left);
+                }
+                res.add(head.val);
+            }
+        }
+        return res;
+    }
+}
