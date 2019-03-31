b6ef37b555823efef9c2ea354fde3befd4c3ecc3
zhuochenhust <zhuochenuva@gmail.com>
2019-03-31 18:14:20 -0400

    Solution 077

---
+++ src/backtracking/Subsets_077.java
@@ -0,0 +1,22 @@
+package backtracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Subsets_077 {
+	public List<List<Integer>> subsets(int[] nums) {
+		List<List<Integer>> res = new ArrayList<>();
+		List<Integer> cur = new ArrayList<>();
+		helper(nums, res, cur, 0);
+		return res;
+	}
+
+	private void helper(int[] nums, List<List<Integer>> res, List<Integer> cur, int start) {
+		res.add(new ArrayList<>(cur));
+		for (int i = start; i < nums.length; i ++) {
+			cur.add(nums[i]);
+			helper(nums, res, cur, i + 1);
+			cur.remove(cur.size() - 1);
+		}
+	}
+}
