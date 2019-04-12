9903c55ccb8966ec69e922bb6235b1ec9bcc2af7
zhuochen <zhuochen@ah-zhuochen.mshome.net>
2019-04-12 14:52:35 -0400

    improve solution 106

--- src/tree/ConstructBinaryTreefromInorderandPostorderTraversal_106.java
+++ src/tree/ConstructBinaryTreefromInorderandPostorderTraversal_106.java
@@ -8,7 +8,10 @@ import java.util.Arrays;
  **/
 
 public class ConstructBinaryTreefromInorderandPostorderTraversal_106 {
-	public TreeNode buildTree(int[] inorder, int[] postorder) {
+	/*
+	 * Solution 1: 每次递归用到的数组是原数组的一部分，每次创建新的数组，但是会浪费额外空间
+	 * */
+	public TreeNode buildTree_1(int[] inorder, int[] postorder) {
 		if (inorder == null || inorder.length == 0 || postorder == null || postorder.length == 0)
 			return null;
 		// 1. 后序遍历数组的最后一个值必然为root的值
@@ -20,8 +23,8 @@ public class ConstructBinaryTreefromInorderandPostorderTraversal_106 {
 		int[] leftpostorder = Arrays.copyOfRange(postorder, 0, inorderrootPosition);
 		int[] rightinorder = Arrays.copyOfRange(inorder, inorderrootPosition + 1, inorder.length);
 		int[] rightpostorder = Arrays.copyOfRange(postorder, inorderrootPosition, postorder.length - 1);
-		TreeNode leftTree = buildTree(leftinorder, leftpostorder);
-		TreeNode rightTree = buildTree(rightinorder, rightpostorder);
+		TreeNode leftTree = buildTree_1(leftinorder, leftpostorder);
+		TreeNode rightTree = buildTree_1(rightinorder, rightpostorder);
 		tree.left = leftTree;
 		tree.right = rightTree;
 		return tree;
@@ -35,4 +38,25 @@ public class ConstructBinaryTreefromInorderandPostorderTraversal_106 {
 		}
 		return -1;
 	}
+	
+	/* 
+	 * Solution 2: 每次递归需要改变所要用到的数组时候，我们要考虑用参数start & end 而不是每次创建新的数组
+	 * 任何时候遇到递归需要用某一部分数组的情况，优先考虑用参数标定所用到数组边界的方法，而不是创建数组
+	 * */
+	public TreeNode buildTree_2(int[] inorder, int[] postorder) {
+        if (inorder == null || postorder == null || inorder.length == 0 || postorder.length == 0) return null;
+        return helper(inorder, postorder, 0, inorder.length - 1, 0, postorder.length - 1);
+    }
+    public TreeNode helper(int[] inorder, int[] postorder, int in_st, int in_end, int post_st, int post_end) {
+        if (in_st > in_end || post_st > post_end) return null;
+        TreeNode root = new TreeNode(postorder[post_end]);
+        int i = in_st;
+        while (i <= in_end) {
+            if (inorder[i] == postorder[post_end]) break;
+            i ++;
+        }
+        root.left = helper(inorder, postorder, in_st, i - 1, post_st, post_st + (i - in_st) - 1);
+        root.right = helper(inorder, postorder, i + 1, in_end, post_st + (i - in_st), post_end - 1);
+        return root;
+    }
 }
