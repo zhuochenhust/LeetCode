9c506a47c93d9040ad32dcf002b18115f1116fca
zhuochenhust <zhuochenuva@gmail.com>
2019-04-21 00:23:16 -0400

    Solution199

---
+++ src/tree/BinaryTreeRightSideView_199.java
@@ -0,0 +1,31 @@
+package tree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class BinaryTreeRightSideView_199 {
+	public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) return res;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<TreeNode> cur = new ArrayList<>();
+            for (int i = 0; i < size; i ++) {
+                TreeNode head = queue.poll();
+                if (head.left != null) {
+                    queue.offer(head.left);
+                }
+                if (head.right != null) {
+                    queue.offer(head.right);
+                }
+                cur.add(head);
+            }
+            res.add(cur.get(cur.size() - 1).val);
+        }
+        return res;
+    }
+}
