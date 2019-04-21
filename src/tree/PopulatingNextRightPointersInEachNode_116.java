2ab7d14c27b21069a610fb7101cb73808f6e9438
zhuochenhust <zhuochenuva@gmail.com>
2019-04-20 21:42:15 -0400

    Solution 116

--- src/tree/PopulatingNextRightPointersInEachNode_116.java
+++ src/tree/PopulatingNextRightPointersInEachNode_116.java
@@ -7,7 +7,7 @@ import java.util.Queue;
 
 public class PopulatingNextRightPointersInEachNode_116 {
 	// 基本思路：BFS
-	public Node connect(Node root) {
+	public Node connect_1(Node root) {
         if (root == null) return root;
         Queue<Node> queue = new LinkedList<>();
         queue.offer(root);
@@ -33,5 +33,23 @@ public class PopulatingNextRightPointersInEachNode_116 {
         	}
         }
         return root;
+    }
+	
+	// 进阶思路：递归
+	public Node connect_2(Node root) {
+        if (root == null) return null;
+        root.next = null;
+        connect(root.left, root.right);
+        return root;
+    }
+    
+    public void connect(Node left, Node right) {
+        // 由于是perfect binary tree，所以如果left是空就说明这一层是空的不需要连接了
+        if (left == null) return; 
+        left.next = right;
+        right.next = null;
+        connect(left.left, left.right);
+        connect(left.right, right.left);
+        connect(right.left, right.right);
     }
 }
