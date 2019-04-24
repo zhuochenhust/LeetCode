7a52b1bbcb3dae07d7b2d1c90f89ceff7c2d7d20
zhuochen <zhuochen@ah-zhuochen.mshome.net>
2019-04-23 20:50:58 -0400

    Solution 654 improvement

--- src/tree/MaximumBinaryTree_654.java
+++ src/tree/MaximumBinaryTree_654.java
@@ -9,23 +9,26 @@ import org.junit.Test;
  * */
 public class MaximumBinaryTree_654 {
 	public TreeNode constructMaximumBinaryTree(int[] nums) {
-        return contructHelper(nums, 0, nums.length);
+        if (nums.length == 0 || nums == null) return null;
+        return helper(nums, 0, nums.length - 1);
     }
-	// 之所以设置l和r两个边界，就是为了避免数组本身的变化。数组可以是不变的，运用规则的范围变化就可以了
-	public TreeNode contructHelper(int[] nums, int l, int r) {
-		if (l == r) return null;
-		int max_i = findMaxIndex(nums, l, r);
-		TreeNode root = new TreeNode(nums[max_i]);
-		root.left = contructHelper(nums, l, max_i);
-		root.right = contructHelper(nums, max_i + 1, r);
-		return root;
-	}
+    
+    // 之所以设置l和r两个边界，就是为了避免数组本身的变化。数组可以是不变的，运用规则的范围变化就可以了
+    public TreeNode helper(int[] nums, int l, int r) {
+        if (l > r) return null;
+        int max_i = findMaxIndex(nums, l, r);
+        TreeNode root = new TreeNode(nums[max_i]);
+        root.left = helper(nums, l, max_i - 1);
+        root.right = helper(nums, max_i + 1, r);
+        return root;
+    }
+    
     public int findMaxIndex(int[] nums, int l, int r) {
         int max_i = l;
-        for (int i = l; i < r; i ++) {
-           if (nums[i] > nums[max_i]) {
-        	   max_i = i;
-           }
+        for (int i = l; i <= r; i ++) {
+            if (nums[i] > nums[max_i]) {
+                max_i = i;
+            }
         }
         return max_i;
     }
