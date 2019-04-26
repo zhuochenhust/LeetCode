c114ed8f6a12c576f821fed355184714cbdcb382
zhuochenhust <zhuochenuva@gmail.com>
2019-04-25 23:18:18 -0400

    Solution 437

--- src/tree/PathSumIII_437.java
+++ src/tree/PathSumIII_437.java
@@ -1,9 +1,10 @@
 package tree;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class PathSumIII_437 {
-	public int pathSum(TreeNode root, int sum) {
+	public int pathSum_1(TreeNode root, int sum) {
         HashMap<Integer, Integer> preSum = new HashMap<>();
         preSum.put(0, 1); // 为何一定要这一步? 因为如果curSum = target，必然count可以加1，所以要把curSum - target = 0考虑进去
         helper(root, sum, 0, preSum);
@@ -31,5 +32,49 @@ public class PathSumIII_437 {
         // 因为backtracking永远是回退递归调用中改变的那个东西，每次调用递归结束要把它回退到调用前的状态
         // 这里的helper改变的是preSum这个hashmap，所以需要把它回退到改变前的状态。
         preSum.put(curSum, preSum.get(curSum) - 1); 
+    }
+    
+    ///////////////////////
+    public int pathSum_2(TreeNode root, int sum) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);  //Default sum = 0 has one count
+        return backtrack(root, 0, sum, map); 
+    }
+    //BackTrack one pass
+    public int backtrack(TreeNode root, int sum, int target, Map<Integer, Integer> map){
+        if(root == null)
+            return 0;
+        sum += root.val;
+        int res = map.getOrDefault(sum - target, 0);    //See if there is a subarray sum equals to target
+        map.put(sum, map.getOrDefault(sum, 0)+1);
+        //Extend to left and right child
+        res += backtrack(root.left, sum, target, map) + backtrack(root.right, sum, target, map);
+        map.put(sum, map.get(sum)-1);   //Remove the current node so it wont affect other path
+        return res;
+    }
+    
+    
+    ////////////////////
+    /**
+     * Each time find all the path start from current node
+     * Then move start node to the child and repeat.
+     * Time Complexity should be O(N^2) for the worst case and O(NlogN) for balanced binary Tree.
+     * */
+    public int pathSum_3(TreeNode root, int sum) {
+        if(root == null)
+            return 0;
+        return findPath(root, sum) + pathSum_3(root.left, sum) + pathSum_3(root.right, sum);
+    }
+    
+    // find all the path count start from current node
+    public int findPath(TreeNode root, int sum){
+        int count = 0;
+        if(root == null)
+            return count;
+        if(sum == root.val)
+        	count++;
+        count += findPath(root.left, sum - root.val);
+        count += findPath(root.right, sum - root.val);
+        return count;
     }
 }
