a8680a1d630b1b5bbc2187faae68572d3ace74d9
zhuochenhust <zhuochenuva@gmail.com>
2019-05-02 23:37:33 -0400

    Solution 98

--- src/tree/ValidateBinarySearchTree_098.java
+++ src/tree/ValidateBinarySearchTree_098.java
@@ -12,13 +12,27 @@ public class ValidateBinarySearchTree_098 {
 		if (root == null) return true;
 		return helper(root, null, null);
 	}
-	//很自然想到要有上下边界，但是用int的话无法确认初始值到底是什么，所以用了Integer
-	public boolean helper(TreeNode root, Integer lower_limit, Integer upper_limit) {
-		if (lower_limit != null && root.val <= lower_limit) return false;
-		if (upper_limit != null && root.val >= upper_limit) return false;
-		boolean left = (root.left != null) ? helper(root.left, lower_limit, root.val) : true;
-		boolean right = (root.right != null) ? helper(root.right, root.val, upper_limit) : true;
-		return (left && right);
+	
+	/**
+	 * 直接思路：
+	 * 1. root.left是不是BST？
+	 * 2. root.right是不是BST
+	 * 3. root.left最大值是不是小于root.val
+	 * 4. root.right最小值是不是大于root.val
+	 * 很不好写，因为我们还得知道root.left最大值，root.right最小值是啥
+	 * ------------------------------------------------------
+	 * 换个思路：
+	 * 对一个节点(root.val = V)来说，我先给他一个范围(min, max),看当前节点值是不是在范围之内
+	 * 		如果否，则返回false
+	 * 		如果是，则root.left的范围是[min, V - 1]; root.right范围是[V + 1, max]
+	 * 用这样的方法来做，我们的返回值只需要boolean类型就可以了
+	 * */
+	public boolean helper(TreeNode root, Integer max, Integer min) {
+		if (root == null) return true;
+		if ((max != null && root.val >= max) || (min != null && root.val <= min)) return false;
+		boolean left = helper(root.left, root.val, min);
+		boolean right = helper(root.right, max, root.val);
+		return left && right;
 	}
 	
 	@Test
