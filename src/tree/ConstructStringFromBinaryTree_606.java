e5c79c450e17cedfacc1ab405c67cde3eb1f4cf1
zhuochenhust <zhuochenuva@gmail.com>
2019-05-06 22:19:01 -0400

    Solution 606

---
+++ src/tree/ConstructStringFromBinaryTree_606.java
@@ -0,0 +1,12 @@
+package tree;
+
+public class ConstructStringFromBinaryTree_606 {
+	public String tree2str(TreeNode t) {
+        if (t == null) return "";
+        if (t.left == null && t.right == null) return t.val + "";
+        if (t.right == null) {
+            return t.val + "(" + tree2str(t.left) + ")";
+        }
+        return t.val + "(" + tree2str(t.left) + ")(" + tree2str(t.right) + ")";
+    }
+}
