d5df683f1f3fc6ef9389d00d30478b28f8ef7f4c
zhuochen <zhuochen@ah-zhuochen.dhcp.mathworks.com>
2019-05-22 14:58:17 -0400

    Solution122

---
+++ src/array/BestTimeToBuyAndSellStockII_122.java
@@ -0,0 +1,23 @@
+package array;
+
+public class BestTimeToBuyAndSellStockII_122 {
+	public int maxProfit(int[] prices) {
+        if (prices == null || prices.length < 2) return 0;
+        int i = 0;
+        int peak = prices[0], valley = prices[0];
+        int maxProfit = 0;
+        while (i < prices.length - 1) {
+            // 这里注意两个while循环的顺序不能反，永远是先找低的点入，再找高的点出。
+            while (i < prices.length - 1 && prices[i] >= prices[i + 1]) {
+                i ++;
+            }
+            valley = prices[i];
+            while (i < prices.length - 1 && prices[i] <= prices[i + 1]) {
+                i ++;
+            }
+            peak = prices[i];            
+            maxProfit += peak - valley;
+        }
+        return maxProfit;
+    }
+}
