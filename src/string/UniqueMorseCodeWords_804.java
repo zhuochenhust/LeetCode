f9f8b28ec33b39cd870d56c1ff2978746bc04759
zhuochendatusi <zhuochenuva@gmail.com>
2019-07-22 16:02:14 -0400

    Update UniqueMorseCodeWords_804.java

--- src/string/UniqueMorseCodeWords_804.java
+++ src/string/UniqueMorseCodeWords_804.java
@@ -1,17 +1,24 @@
+package string;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class UniqueMorseCodeWords_804 {
-    public static final String[] morse = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---",
-                          "-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-",
-                          "...-",".--","-..-","-.--","--.."};
-    public int uniqueMorseRepresentations(String[] words) {    
-        if (words == null || words.length == 0) return 0;
-        Set<String> res = new HashSet<>();
-        for (String word : words) {
-            StringBuilder sb = new StringBuilder();
-            for (char c : word.toCharArray()) {
-                sb.append(morse[c - 'a']);
-            }
-            res.add(sb.toString());
-        }
-        return res.size();
-    }
+	public static final String[] morse = { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-",
+			".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--",
+			"--.." };
+
+	public int uniqueMorseRepresentations(String[] words) {
+		if (words == null || words.length == 0)
+			return 0;
+		Set<String> res = new HashSet<>();
+		for (String word : words) {
+			StringBuilder sb = new StringBuilder();
+			for (char c : word.toCharArray()) {
+				sb.append(morse[c - 'a']);
+			}
+			res.add(sb.toString());
+		}
+		return res.size();
+	}
 }
