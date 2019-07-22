c4c189d7ee38abf4f66f07da9a0a441f219416ba
zhuochendatusi <zhuochenuva@gmail.com>
2019-07-22 14:49:03 -0400

    Create UniqueMorseCodeWords_804.java

---
+++ src/string/UniqueMorseCodeWords_804.java
@@ -0,0 +1,17 @@
+public class UniqueMorseCodeWords_804 {
+    public static final String[] morse = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---",
+                          "-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-",
+                          "...-",".--","-..-","-.--","--.."};
+    public int uniqueMorseRepresentations(String[] words) {    
+        if (words == null || words.length == 0) return 0;
+        Set<String> res = new HashSet<>();
+        for (String word : words) {
+            StringBuilder sb = new StringBuilder();
+            for (char c : word.toCharArray()) {
+                sb.append(morse[c - 'a']);
+            }
+            res.add(sb.toString());
+        }
+        return res.size();
+    }
+}
