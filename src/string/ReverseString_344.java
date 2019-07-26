ebff62afef7219b09c0c7243e21cec18d8a5ffd6
zhuochendatusi <zhuochenuva@gmail.com>
2019-07-26 09:51:37 -0400

    Update ReverseString_344.java

--- src/string/ReverseString_344.java
+++ src/string/ReverseString_344.java
@@ -16,4 +16,18 @@ public class ReverseString_344 {
         s[i] = s[j];
         s[j] = tmp;
     }
+	
+	public void reverseString_2(char[] s) {
+        if (s == null || s.length == 0) return;
+        // int i = 0, j = s.length- 1;
+        StringBuilder sb = new StringBuilder();
+        for (int i = s.length - 1; i >= 0; i --) {
+            sb.append(s[i]);
+        }
+        char[] c = sb.toString().toCharArray();
+        for (int i = 0; i < s.length; i ++) {
+            s[i] = c[i];
+        }
+        return;
+    }
 }
