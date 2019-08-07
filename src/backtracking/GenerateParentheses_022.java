5fd731aae08eed127d8e69f60fcb20df88daef04
zhuochendatusi <zhuochenuva@gmail.com>
2019-08-07 19:51:50 -0400

    Update GenerateParentheses_022.java

--- src/backtracking/GenerateParentheses_022.java
+++ src/backtracking/GenerateParentheses_022.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GenerateParentheses_022 {
+	// Using StringBuilder
 	public List<String> generateParenthesis(int n) {
 		List<String> res = new ArrayList<>();
 		if (n <= 0) return res;
@@ -26,6 +27,32 @@ public class GenerateParentheses_022 {
 			sb.append(')');
 			helper(res, sb, open_left, close_left - 1);
 			sb.deleteCharAt(sb.length() - 1);
+		}
+	}
+	
+	// Using String instead of StringBuilder
+	public List<String> generateParenthesis_2(int n) {
+		List<String> res = new ArrayList<>();
+		if (n <= 0) return res;
+		//StringBuilder sb = new StringBuilder();
+		helper(res, "", n, n);
+		return res;
+	}
+	
+	public void helper_2(List<String> res, String s, int open_left, int close_left) {
+		if (open_left == 0 && close_left == 0) {
+			res.add(s);
+			return;
+		}
+		if (open_left > 0) {
+			// sb.append('(');
+			helper(res, s + "(", open_left - 1, close_left);
+			// sb.deleteCharAt(sb.length() - 1);
+		}
+		if (close_left > open_left) {
+			// sb.append(')');
+			helper(res, s + ")", open_left, close_left - 1);
+			// sb.deleteCharAt(sb.length() - 1);
 		}
 	}
 }
