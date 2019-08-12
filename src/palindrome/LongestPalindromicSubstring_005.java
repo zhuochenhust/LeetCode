273f624acaacfd7ec44e95c393acdef3e2958dac
zhuochen <zhuochen@zhuos-air.fios-router.home>
2019-08-11 21:18:39 -0400

    go

--- src/palindrome/LongestPalindromicSubstring_005.java
+++ src/palindrome/LongestPalindromicSubstring_005.java
@@ -1,7 +1,8 @@
 package palindrome;
 
 public class LongestPalindromicSubstring_005 {
-	public String longestPalindrome(String s) {
+	// 常规解法：
+	public String longestPalindrome_1(String s) {
         if (s == null || s.length() == 0) return "";
         String result = "";
         for (int i = 0; i < s.length(); i ++) {
@@ -25,5 +26,47 @@ public class LongestPalindromicSubstring_005 {
             right ++;
         }
         return s.substring(left + 1, right);
+    }
+    
+    // 动态规划解法：
+    public String longestPalindrome_2(String s) {
+        if (s == null) return "";
+        int n = s.length();
+        if (n == 0) return "";
+        if (n == 1) return s;
+        boolean[][] dp = new boolean[n][n];
+        
+        // 初始化dp
+        for (int i = 0; i < n; i ++) {
+            for (int j = 0; j < n; j ++) {
+                // i > j 是空串，回文，i == j，单字符串，回文
+                // 其他情况就是字符串数组默认初始化的false
+                if (i >= j) {
+                    dp[i][j] = true;
+                }
+            }
+        }
+        
+        // 构建dp的推导公式
+        int maxLen = 1, left = 0, right = 0;
+        for (int l = 1; l < n; l ++) {
+            for (int i = 0; i + l < n; i ++) {
+                int j = i + l;
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = dp[i + 1][j - 1];
+                    if (dp[i][j]) { // i～j是回文
+                        if (l + 1 > maxLen) { // 当前回文字符串长度大于maxLen则更新maxLen和左右指针
+                            maxLen = l + 1;
+                            left = i;
+                            right = j;
+                        }
+                    }
+                    
+                } else {
+                    dp[i][j] = false;
+                }
+            }
+        }
+        return s.substring(left, right + 1);
     }
 }
