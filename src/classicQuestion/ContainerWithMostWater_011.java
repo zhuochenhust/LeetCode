91e2ec184c9c8cb2e5ec3ed47a308d75e9418e68
zhuochen <zhuochen@zhuos-air.fios-router.home>
2019-08-21 19:27:44 -0400

    11

---
+++ src/classicQuestion/ContainerWithMostWater_011.java
@@ -0,0 +1,15 @@
+package classicQuestion;
+
+public class ContainerWithMostWater_011 {
+	public int maxArea(int[] height) {
+        int ans = 0, l = 0, r = height.length - 1;
+        while (l < r) {
+            ans = Math.max(ans, Math.min(height[l], height[r]) * (r - l));
+            if (height[l] < height[r])
+                l++;
+            else
+                r--;
+        }
+        return ans;
+    }
+}
